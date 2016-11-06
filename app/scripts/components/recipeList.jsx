081668fb44b398061393ebc79ba8fd7cbf83f1b8
Luis Rene Onate <lonate12@gmail.com>
2016-11-06 09:53:45 -0500

    Updated recipe list component to fetch data and produce list items.

--- app/scripts/components/recipeList.jsx
+++ app/scripts/components/recipeList.jsx
@@ -1,12 +1,30 @@
 var React = require('react');
 
 var Template = require('./layout/template.jsx').Template;
+var RecipeCollection = require('../models/recipes.js').RecipeCollection;
 
 var RecipeList = React.createClass({
+  getInitialState: function(){
+    var self = this;
+    var recipeCollection = new RecipeCollection();
+    recipeCollection.fetch().then(function(){
+      self.setState({recipeCollection: self.state.recipeCollection});
+    });
+
+    return{
+      recipeCollection: recipeCollection
+    };
+  },
   render: function(){
+    var recipes = this.state.recipeCollection.map(function(recipe){
+      return (
+        <h1>This is a recipe</h1>
+      );
+    });
+
     return(
       <Template>
-        <h1>Test Recipe List</h1>
+        {recipes}
       </Template>
     );
   }
