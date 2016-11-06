202b5d8e023fe5b9cdbe12429ddea856981c66e0
Luis Rene Onate <lonate12@gmail.com>
2016-11-06 15:10:34 -0500

    Added ability to retrieve recipes on server and display.

--- app/scripts/components/recipeList.jsx
+++ app/scripts/components/recipeList.jsx
@@ -3,12 +3,34 @@ var React = require('react');
 var Template = require('./layout/template.jsx').Template;
 var RecipeCollection = require('../models/recipes.js').RecipeCollection;
 
+var RecipeTile = React.createClass({
+  render: function(){
+    var tiles = this.props.recipeCollection.map(function(recipe){
+      return(
+        <p className="recipe-name" key={recipe.get('objectId')}>{recipe.get('recipeName')}</p>
+      );
+    });
+    return(
+      <div className="recipe-tile">
+        {tiles}
+      </div>
+    );
+  }
+});
+
 var RecipeListContainer = React.createClass({
   getInitialState: function(){
     var self = this;
     var recipeCollection = new RecipeCollection();
-    recipeCollection.fetch().then(function(){
-      self.setState({recipeCollection: self.state.recipeCollection});
+    recipeCollection.fetch().then(function(data){
+      recipeCollection.reset([])
+      var recipes = data.results;
+
+      recipes.forEach(function(recipe){
+        recipeCollection.add(recipe);
+      });
+
+      self.setState({recipeCollection: recipeCollection});
     });
 
     return{
@@ -16,15 +38,11 @@ var RecipeListContainer = React.createClass({
     };
   },
   render: function(){
-    var recipes = this.state.recipeCollection.map(function(recipe){
-      return (
-        <h1>This is an ingredient</h1>
-      );
-    });
+    console.log(this.state.recipeCollection);
 
     return(
       <Template>
-        {recipes}
+        <RecipeTile recipeCollection={this.state.recipeCollection}/>
       </Template>
     );
   }
--- app/scripts/models/recipes.js
+++ app/scripts/models/recipes.js
@@ -1,7 +1,7 @@
 var Backbone = require('backbone');
 
 var Recipe = Backbone.Model.extend({
-
+  idAttribute: 'objectId'
 });
 
 var RecipeCollection = Backbone.Collection.extend({
